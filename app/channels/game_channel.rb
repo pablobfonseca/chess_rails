8035fdbecea37196f2e845ec6771bacea6e438be
Pablo Fonseca <pablofonseca777@gmail.com>
2016-08-14 23:49:59 -0300

    Create game channel

---
+++ app/channels/game_channel.rb
@@ -0,0 +1,6 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+class GameChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "player_#{uuid}"
+  end
+end
